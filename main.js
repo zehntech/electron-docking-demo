69fc9b731d7474dae0df5c33de7b52302582466f
ztap0011 <abhay.patidar@zehntech.com>
2019-04-03 18:25:53 +0530

    formatted the code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const webContents = electron.webContents;
 // Module to control application life.
 const app = electron.app
-const ipcMain=electron.ipcMain;
+const ipcMain = electron.ipcMain;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -13,9 +13,9 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -35,54 +35,56 @@ function createWindow () {
     mainWindow = null
   });
 
-  
+
   mainWindow.maximize();
- 
+
 }
 var mainSender;
 
-ipcMain.on('test-call',(e,ar)=>{
+ipcMain.on('test-call', (e, ar) => {
   console.log("test call");
 })
-ipcMain.on('doc-window', (event, arg1) => {  
+ipcMain.on('doc-window', (event, arg1) => {
   // let w1=webContents.fromId('tab1-webview');
   // w1.executeJavaScript('document.getElementsByTagName("body")[0].innerHTML;', true)
   //   .then((result) => {
   //     console.log(result) // Will be the JSON object from the fetch call
   //   })
   // Print 1
-  console.log("arg"+arg1);
-  mainSender=event.sender;
-  ipcMain.on('is-tab-available',(e,id)=>{
+  console.log("arg" + arg1);
+  mainSender = event.sender;
+  ipcMain.on('is-tab-available', (e, id) => {
     console.log("is tab available");
-    e.sender.send('is-tab-available-reply',{id:id,tab:arg1.tab,tabContent:arg1.tabContent,
-    webviewContent:arg1.webviewContent});
+    e.sender.send('is-tab-available-reply', {
+      id: id, tab: arg1.tab, tabContent: arg1.tabContent,
+      webviewContent: arg1.webviewContent
+    });
   })
-  dockedWindow = new BrowserWindow({width: 800, height: 600})
+  dockedWindow = new BrowserWindow({ width: 800, height: 600 })
   dockedWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
 
-  
-  
+
+
 });
 
-ipcMain.on('undock-start',(e,args)=>{
- // console.log("undock start "+args.top);
+ipcMain.on('undock-start', (e, args) => {
+  // console.log("undock start "+args.top);
   //console.log("is visible "+mainWindow.isVisible());
-  if(mainWindow.isVisible() && args.top<=-60 && dockedWindow){
+  if (mainWindow.isVisible() && args.top <= -60 && dockedWindow) {
     mainSender.send('add-new-tab');
-    console.log("undock start "+args.top);
+    console.log("undock start " + args.top);
     //console.log("webcontents "+JSON.stringify(dockedWindow.webContents));
     dockedWindow.close();
-    dockedWindow=null;
-  }else{
-   // console.log("m "+JSON.stringify(mainWindow.getBounds()));
-   // console.log("d "+JSON.stringify(dockedWindow.getBounds()));
+    dockedWindow = null;
+  } else {
+    // console.log("m "+JSON.stringify(mainWindow.getBounds()));
+    // console.log("d "+JSON.stringify(dockedWindow.getBounds()));
   }
-  
+
 })
 
 
